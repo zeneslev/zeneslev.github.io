4a375551bbe36d7e2d2f7f9ca83acc901b9c3033
zeneslev <mattkrolick@gmail.com>
2025-01-31 19:35:26 -0500

    added mateo page

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,5 +1,11 @@
 var store = [{
         "title": "First Post",
+        "excerpt":"Hello Mateo  ","categories": [],
+        "tags": ["blogpost"],
+        "url": "/2025/01/26/NextPost.html",
+        "teaser": null
+      },{
+        "title": "First Post",
         "excerpt":"This is just a test. Standby.     ","categories": [],
         "tags": ["blogpost"],
         "url": "/2025/01/26/SecondPost.html",
